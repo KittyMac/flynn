e737771cfa7d0c646bde2097f1b952934b829831
Rocco Bowling <rocco@chimerasw.com>
2022-08-08 14:59:47 -0400

    rework how dependencies are processed

--- Plugins/FlynnPlugin/FlynnPlugin.swift
+++ Plugins/FlynnPlugin/FlynnPlugin.swift
@@ -3,37 +3,27 @@ import PackagePlugin
 
 @main struct FlynnPlugin: BuildToolPlugin {
     
-    func gatherSwiftInputFiles(target: Target,
+    func gatherSwiftInputFiles(targets: [Target],
                                isRoot: Bool,
                                inputFiles: inout [PackagePlugin.Path]) {
         
-        let url = URL(fileURLWithPath: target.directory.string)
-        if let enumerator = FileManager.default.enumerator(at: url,
-                                                           includingPropertiesForKeys: [.isRegularFileKey],
-                                                           options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
-            for case let fileURL as URL in enumerator {
-                do {
-                    let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
-                    if fileAttributes.isRegularFile == true && fileURL.pathExtension == "swift" {
-                        if isRoot {
-                            inputFiles.append(PackagePlugin.Path(fileURL.path))
-                        } else {
-                            inputFiles.append(PackagePlugin.Path("+" + fileURL.path))
+        for target in targets {
+            let url = URL(fileURLWithPath: target.directory.string)
+            if let enumerator = FileManager.default.enumerator(at: url,
+                                                               includingPropertiesForKeys: [.isRegularFileKey],
+                                                               options: [.skipsHiddenFiles, .skipsPackageDescendants]) {
+                for case let fileURL as URL in enumerator {
+                    do {
+                        let fileAttributes = try fileURL.resourceValues(forKeys:[.isRegularFileKey])
+                        if fileAttributes.isRegularFile == true && fileURL.pathExtension == "swift" {
+                            if isRoot {
+                                inputFiles.append(PackagePlugin.Path(fileURL.path))
+                            } else {
+                                inputFiles.append(PackagePlugin.Path("+" + fileURL.path))
+                            }
                         }
-                    }
-                } catch { print(error, fileURL) }
-            }
-        }
-        
-        for dependency in target.dependencies {
-            switch dependency {
-            case .target(let target):
-                gatherSwiftInputFiles(target: target,
-                                      isRoot: false,
-                                      inputFiles: &inputFiles)
-                break
-            default:
-                break
+                    } catch { print(error, fileURL) }
+                }
             }
         }
     }
@@ -43,13 +33,17 @@ import PackagePlugin
         guard let target = target as? SwiftSourceModuleTarget else {
             return []
         }
-
+        
         let tool = try context.tool(named: "FlynnLint")
-                
+        
         // Find all .swift files in our target and all of our target's dependencies, add them as input files
         var inputFiles: [PackagePlugin.Path] = []
-        gatherSwiftInputFiles(target: target,
+        
+        gatherSwiftInputFiles(targets: [target],
                               isRoot: true,
+                              inputFiles: &inputFiles)
+        gatherSwiftInputFiles(targets: target.recursiveTargetDependencies,
+                              isRoot: false,
                               inputFiles: &inputFiles)
                 
         let inputFilesFilePath = context.pluginWorkDirectory.string + "/inputFiles.txt"
