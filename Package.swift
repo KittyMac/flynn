23dd28aeee00132e23a91471745fc2630b64e674
Rocco Bowling <rocco@chimerasw.com>
2022-09-01 17:14:13 -0400

    add Pony as a library

--- Package.swift
+++ Package.swift
@@ -49,6 +49,7 @@ let package = Package(
     ],
     products: productsTarget + [
         .library(name: "Flynn", targets: ["Flynn"]),
+        .library(name: "Pony", targets: ["Pony"]),
         .plugin(name: "FlynnPlugin", targets: ["FlynnPlugin"])
     ],
     dependencies: pluginDependencies + [
