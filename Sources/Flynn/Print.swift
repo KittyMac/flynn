cb87c10a9f8c04a394df654a64b40ed833da3011
Rocco Bowling <rocco@chimerasw.com>
2025-06-12 07:28:05 -0400

    squash all prints unless DEBUG

--- Sources/Flynn/Print.swift
+++ Sources/Flynn/Print.swift
@@ -3,13 +3,17 @@ import Foundation
 @usableFromInline
 func print(_ items: Any..., separator: String = " ", terminator: String = "\n", truncate: Int) {
     let output = items.map { "\($0)" }.joined(separator: separator)
+#if DEBUG
     Flynn.syslog("Flynn", output)
+#endif
 }
 
 @usableFromInline
 func print(tag: String, _ items: Any..., separator: String = " ", terminator: String = "\n", truncate: Int) {
     let output = items.map { "\($0)" }.joined(separator: separator)
+#if DEBUG
     Flynn.syslog(tag, output)
+#endif
 }
 
 @usableFromInline
