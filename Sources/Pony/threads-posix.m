ebd80df96fa7f92fd28aaa0c8ad09076121adf24
Rocco Bowling <rocco@chimerasw.com>
2026-07-17 09:56:12 -0400

    set 8MB stack size for flynn scheduler threads

--- Sources/Pony/threads-posix.m
+++ Sources/Pony/threads-posix.m
@@ -17,6 +17,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #ifdef PLATFORM_IS_APPLE
 #undef id
@@ -53,31 +54,48 @@ void ponyint_mutex_unlock(PONY_MUTEX mutex) {
 bool ponyint_thread_create(pony_thread_id_t* thread, thread_fn start, int qos, void* arg)
 {
     bool ret = true;
-    
-    bool setstack_called = false;
-    struct rlimit limit;
+
     pthread_attr_t attr;
-    pthread_attr_t* attr_p = &attr;
-    pthread_attr_init(attr_p);
-    
-    // Some systems, e.g., macOS, hav a different default default
-    // stack size than the typical system's RLIMIT_STACK.
-    // Let's use RLIMIT_STACK's current limit if it is sane.
-    if(getrlimit(RLIMIT_STACK, &limit) == 0 &&
-       limit.rlim_cur != RLIM_INFINITY &&
-       limit.rlim_cur >= PTHREAD_STACK_MIN)
-    {
-        if(! setstack_called)
-            pthread_attr_setstacksize(&attr, (size_t)limit.rlim_cur);
-    } else {
-        attr_p = NULL;
+    pthread_attr_init(&attr);
+
+    // Start from RLIMIT_STACK's soft limit when it is sane...
+    size_t desired = 0;
+    struct rlimit limit;
+    if (getrlimit(RLIMIT_STACK, &limit) == 0 &&
+        limit.rlim_cur != RLIM_INFINITY &&
+        limit.rlim_cur >= (rlim_t)PTHREAD_STACK_MIN) {
+        desired = (size_t)limit.rlim_cur;
     }
-    
+
+    // ...but never below our floor
+    size_t floor = (8 * 1024 * 1024);
+    if (desired < floor) {
+        desired = floor;
+    }
+
+    // pthread_attr_setstacksize requires a value that is >= PTHREAD_STACK_MIN
+    // AND a multiple of the page size (16 KB on Apple Silicon, not 4 KB),
+    // else it returns EINVAL and the attr silently keeps its small default.
+    long page = sysconf(_SC_PAGESIZE);
+    if (page <= 0) page = 4096;
+    if (desired < (size_t)PTHREAD_STACK_MIN)
+        desired = (size_t)PTHREAD_STACK_MIN;
+    desired -= (desired % (size_t)page);
+    if (desired < (size_t)PTHREAD_STACK_MIN)
+        desired += (size_t)page;
+
+    int rc = pthread_attr_setstacksize(&attr, desired);
+    if (rc != 0) {
+        fprintf(stderr,
+            "flynn: pthread_attr_setstacksize(%zu) failed (rc=%d); "
+            "scheduler thread will use the default stack size\n", desired, rc);
+    }
+
 #ifdef PLATFORM_IS_APPLE
     pthread_attr_set_qos_class_np(&attr, qos, 0);
 #endif
-    
-    if(pthread_create(thread, attr_p, start, arg))
+
+    if (pthread_create(thread, &attr, start, arg))
         ret = false;
     pthread_attr_destroy(&attr);
     return ret;
@@ -85,7 +103,7 @@ bool ponyint_thread_create(pony_thread_id_t* thread, thread_fn start, int qos, v
 
 bool ponyint_thread_join(pony_thread_id_t thread)
 {
-    if(pthread_join(thread, NULL))
+    if (pthread_join(thread, NULL))
         return false;
     return true;
 }
