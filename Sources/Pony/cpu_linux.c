b9beb14a2579748fa4abab6748c8ec17ba913679
Rocco Bowling <rocco@chimerasw.com>
2026-08-19 14:45:07 -0400

    remove purge syslogs

--- Sources/Pony/cpu_linux.c
+++ Sources/Pony/cpu_linux.c
@@ -89,13 +89,10 @@ void pony_malloc_trim(size_t pad) {
     // Prefer the all-arena purge; fall back when the header knows M_PURGE_ALL but
     // the running platform does not implement it (mallopt returns 0 on failure).
 #if defined(M_PURGE_ALL)
-    pony_syslog("TAG", "M_PURGE_ALL");
     if (pony_mallopt_fn(M_PURGE_ALL, 0) == 0) {
-        pony_syslog("TAG", " -> M_PURGE");
         pony_mallopt_fn(M_PURGE, 0);
     }
 #elif defined(M_PURGE)
-    pony_syslog("TAG", "M_PURGE");
     pony_mallopt_fn(M_PURGE, 0);
 #endif
 #else
